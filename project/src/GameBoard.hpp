9595bbd27118fcb185e758edb937347eece7748c
leesom60 <84122542+leesom60@users.noreply.github.com>
2022-05-12 20:27:35 +0900

    Add files via upload

---
+++ project/src/GameBoard.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+class GameBoard : public Board{
+    WINDOW *board_win;
+    int height, width;
+    
+public:
+    GameBoard(int height, int width, int y, int x) : Board(height, width, y, x){
+    }
+};
